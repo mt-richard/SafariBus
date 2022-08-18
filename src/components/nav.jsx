4e9d4c7029ec2c02c234eb60befbd70cc7835504
Richard <Richard@festimoltd.com>
2022-08-18 23:13:57 +0200

    my third commit

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -29,9 +29,9 @@ let [menu,setMenu] =useState();
             
                    
                  
-            <div className={` w-100  ${open ? 'bg-white opacity-95 absolute mt-10 responce':' '}`}>
+            <div className={` w-100  ${open ? 'bg-white absolute mt-10 responce':' '}`}>
                       
-                    <ul className={`md:flex justify-between absolute left-0  inks md:flex md:items-center md:px-12 md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] left-0  w-full md:w-auto ${open ? 'top-20 opacity-100':'top-[-490px] opacity-100 '}`}>
+                    <ul className={`md:flex justify-between absolute left-0  inks md:flex md:items-center md:px-12 md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] left-0  w-full md:w-auto ${open ? 'top-20 text-yellow-400 opacity-[1]':'top-[-490px] opacity-100 '}`}>
                         { links.map((e) =>(
                         <li className="md:text-center py-2 px-10 text-gray-500">
                             <a className="text-xl link" href={e.link}>{e.name}</a>
