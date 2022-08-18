026333c9e4e838e56f14c1e46be53c2642838ef9
Richard <Richard@festimoltd.com>
2022-08-18 23:06:40 +0200

    my third commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,12 +9,12 @@ const Home = () => {
                     <h2 className="text-2xl md:text-3xl">Welcome to</h2>
                     <div className="line mt-5 ml-5"></div>
                 </div>
-                <h1 className="text-gray-800 text-2xl md:text-3xl font-bold ">SafariBus platform</h1>
+                <h1 className="text-gray-800 text-2xl md:text-3xl ">SafariBus platform</h1>
                 <p className="w-full">An integrated platform inspired by technology to contribute to lasting solutions.</p>
-                <button className="btn1 text-white w-50 rounded px-5 py-1">Discover More</button>
+                <button className="btn1 text-white w-[150px] md:w-40 rounded px-5 py-1">Discover More</button>
           </div>
           <div className="banner mt-5 w-full">
-            <img src={pic1} alt="Home" srcset="" className="photo w-100 h-[21opx] w-[320px] md:w-[320px] md:h-[210px] "/>
+            <img src={pic1} alt="Home" srcset="" className="photo w-100 h-[200px] w-[320px] md:w-[320px] md:h-[210px] "/>
             <div className="btns flex">
                 <button className=" text-white rounded  text-[14px] md:text-[15px] md:px-5 py-1  mr-5 md:mr-5">Card Pre-Register</button>
                 <button className=" text-white rounded  text-[14px] md:text-[15px] md:px-5 py-1  mr-5 md:mr-5">Card Topup</button>
